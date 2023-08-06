682edec24dc18ebece670031c36153b037ff8a7e
zkfmapf123 <47292546+zkfmapf123@users.noreply.github.com>
2023-08-07 02:25:31 +0900

    Wip

--- codeDeploy-hooks/index.js
+++ codeDeploy-hooks/index.js
@@ -1,11 +1,9 @@
 'use strict'
 
-const { IncomingWebhook } = require('@slack/webhook')
 const AWS = require('aws-sdk')
 const codedeploy = new AWS.CodeDeploy({ apiVersion: '2014-10-06' })
-const webhook = new IncomingWebhook('https://hooks.slack.com/services/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')
 
-exports.handler = async (event, context, callback) => {
+exports.handler = (event, context, callback) => {
   console.log('Entering AfterAllowTestTraffic hook.')
 
   // Read the DeploymentId and LifecycleEventHookExecutionId from the event payload
@@ -31,16 +29,10 @@ exports.handler = async (event, context, callback) => {
       // Validation failed.
       console.log('AfterAllowTestTraffic validation tests failed')
       console.log(err, err.stack)
-      // webhook.send({
-      //   text: JSON.stringify(err),
-      // })
       callback('CodeDeploy Status update failed')
     } else {
       // Validation succeeded.
       console.log('AfterAllowTestTraffic validation tests succeeded')
-      // webhook.send({
-      //   text: JSON.stringify(data),
-      // })
       callback(null, 'AfterAllowTestTraffic validation tests succeeded')
     }
   })
