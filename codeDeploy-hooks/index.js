3caed46e8abba6867afb462b6532fd7d58afa307
zkfmapf123 <47292546+zkfmapf123@users.noreply.github.com>
2023-08-07 02:52:00 +0900

    add

--- codeDeploy-hooks/index.js
+++ codeDeploy-hooks/index.js
@@ -3,9 +3,9 @@
 const AWS = require('aws-sdk')
 const { IncomingWebhook } = require('@slack/webhook')
 const codedeploy = new AWS.CodeDeploy({ apiVersion: '2014-10-06' })
-const webhook = new IncomingWebhook('https://hooks.slack.com/services/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')
+const webhook = new IncomingWebhook('https://hooks.slack.com/services/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')
 
-exports.handler = async (event, context, callback) => {
+exports.handler = (event, context, callback) => {
   console.log('Entering AfterAllowTestTraffic hook.')
 
   // Read the DeploymentId and LifecycleEventHookExecutionId from the event payload
@@ -32,20 +32,24 @@ exports.handler = async (event, context, callback) => {
       console.log('AfterAllowTestTraffic validation tests failed')
       console.log(err, err.stack)
 
-      webhook.send({
-        text: err,
-      })
-
       callback('CodeDeploy Status update failed')
     } else {
       // Validation succeeded.
       console.log('AfterAllowTestTraffic validation tests succeeded')
 
-      webhook.send({
-        text: data,
-      })
-
-      callback(null, 'AfterAllowTestTraffic validation tests succeeded')
+      // Send a message to Slack webhook
+      webhook
+        .send({
+          text: JSON.stringify(data, null, 4),
+        })
+        .then(() => {
+          console.log('Slack webhook message sent successfully.')
+          callback(null, 'AfterAllowTestTraffic validation tests succeeded')
+        })
+        .catch((error) => {
+          console.error('Error sending Slack webhook message:', error)
+          callback(null, 'AfterAllowTestTraffic validation tests succeeded, but Slack webhook failed to send.')
+        })
     }
   })
 }
