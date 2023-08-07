8545547848d7f1d52993e26dbb52505f1d4b574b
leedonggyu <mooove@npixel.co.kr>
2023-08-07 11:06:27 +0900

    update

--- codeDeploy-hooks/index.js
+++ codeDeploy-hooks/index.js
@@ -1,20 +1,23 @@
-const AWS = require('aws-sdk')
-const { IncomingWebhook } = require('@slack/webhook')
-const codedeploy = new AWS.CodeDeploy({ apiVersion: '2014-10-06' })
-const webhook = new IncomingWebhook('https://hooks.slack.com/services/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')
+const AWS = require("aws-sdk");
+const { IncomingWebhook } = require("@slack/webhook");
+const codedeploy = new AWS.CodeDeploy({ apiVersion: "2014-10-06" });
+const webhook = new IncomingWebhook(
+  "https://hooks.slack.com/services/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"
+);
 
 exports.handler = async (event, context) => {
-  console.log('Entering AfterAllowTestTraffic hook.')
+  console.log(event.lifecycleEvent);
+  console.log("Entering AfterAllowTestTraffic hook.", event);
 
   // Read the DeploymentId and LifecycleEventHookExecutionId from the event payload
-  const deploymentId = event.DeploymentId
-  const lifecycleEventHookExecutionId = event.LifecycleEventHookExecutionId
-  let validationTestResult = 'Failed'
+  const deploymentId = event.DeploymentId;
+  const lifecycleEventHookExecutionId = event.LifecycleEventHookExecutionId;
+  let validationTestResult = "Failed";
 
   // Perform AfterAllowTestTraffic validation tests here. Set the test result
   // to "Succeeded" for this tutorial.
-  console.log('This is where AfterAllowTestTraffic validation tests happen.')
-  validationTestResult = 'Succeeded'
+  console.log("This is where AfterAllowTestTraffic validation tests happen.");
+  validationTestResult = "Succeeded";
 
   try {
     // Complete the AfterAllowTestTraffic hook by sending CodeDeploy the validation status
@@ -22,33 +25,39 @@ exports.handler = async (event, context) => {
       deploymentId: deploymentId,
       lifecycleEventHookExecutionId: lifecycleEventHookExecutionId,
       status: validationTestResult, // status can be 'Succeeded' or 'Failed'
-    }
+    };
 
     // Pass CodeDeploy the prepared validation test results using async/await.
     await new Promise((resolve, reject) => {
       codedeploy.putLifecycleEventHookExecutionStatus(params, (err, data) => {
         if (err) {
           // Validation failed.
-          console.log('AfterAllowTestTraffic validation tests failed')
-          console.log(err, err.stack)
-          reject('CodeDeploy Status update failed')
+          console.log("AfterAllowTestTraffic validation tests failed");
+          console.log(err, err.stack);
+          reject("CodeDeploy Status update failed");
         } else {
           // Validation succeeded.
-          console.log('AfterAllowTestTraffic validation tests succeeded')
-          resolve('CodeDeploy State Update Succeeded')
+
+          console.log("AfterAllowTestTraffic validation tests succeeded");
+          resolve("CodeDeploy State Update Succeeded");
         }
-      })
-    })
+      });
+    });
 
-    await webhook.send({ text: `${encodingDeployId(deploymentId)} is success` })
+    console.log("hello world >> ", params);
+    console.log(
+      Buffer.from(params.lifecycleEventHookExecutionId, "base64").toString(
+        "utf-8"
+      )
+    );
+    console.log(context);
 
-    return 'Function execution completed successfully'
+    await webhook.send({
+      text: JSON.stringify(params),
+    });
+
+    return "Function execution completed successfully";
   } catch (error) {
-    throw new Error(error)
+    throw new Error(error);
   }
-}
-
-const encodingDeployId = (deploymentId) => {
-  const eventData = JSON.parse(Buffer.from(deploymentId, 'base64').toString('utf-8'))
-  return eventData.lifecycleEvent
-}
+};
